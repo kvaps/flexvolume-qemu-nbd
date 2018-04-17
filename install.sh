59658c650d2ff242e503ff94de0c11d0fb99e118
kvaps <kvapss@gmail.com>
2018-04-17 18:57:30 +0200

    add: loop support

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ set -o pipefail
 
 VENDOR=kvaps
 DRIVER=qemu-nbd
-OLD_DRIVER=nbd
 
 # Install driver
 driver_dir=$VENDOR${VENDOR:+"~"}${DRIVER}
@@ -16,9 +15,11 @@ cp "/$DRIVER" "/flexmnt/$driver_dir/.$DRIVER"
 mv -f "/flexmnt/$driver_dir/.$DRIVER" "/flexmnt/$driver_dir/$DRIVER"
 
 # Update old driver also
-old_driver_dir=$VENDOR${VENDOR:+"~"}${OLD_DRIVER}
-mkdir -p "/flexmnt/$old_driver_dir"
-ln -sf "../$driver_dir/$DRIVER" "/flexmnt/$old_driver_dir/$OLD_DRIVER"
+for LN_DRIVER in nbd loop; do
+    ln_driver_dir=$VENDOR${VENDOR:+"~"}${LN_DRIVER}
+    mkdir -p "/flexmnt/$ln_driver_dir"
+    ln -sf "../$driver_dir/$DRIVER" "/flexmnt/$ln_driver_dir/$LN_DRIVER"
+done
 
 # Sleep calm
 while : ; do
